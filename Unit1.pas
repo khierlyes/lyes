4d8792f8e16c0314f9b79e2a3c298dbb775c1d31
khierlyes <khierlyes@yahoo.fr>
2016-11-08 15:20:17 +0100

    First porject

---
+++ Unit1.pas
@@ -0,0 +1,31 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+showmessage('Saha lyes');
+end;
+
+end.
